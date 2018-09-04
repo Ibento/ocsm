63e938afa7cba3d4ba6e9768658dc4994d89d383
Øyvind Brungot Dahl <oyvind@ibento.no>
2018-09-04 15:53:57 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  $(".content_kids").show();
+  $(".content_teachers").hide();
+  $(".content_researchers").hide();
+
+  $(".content_support").show();
+  $(".content_volunteer").hide();
+  $(".content_internships").hide();
+
+
+  $(".forkids").click(function() {
+    $(".content_kids").show();
+    $(".content_teachers").hide();
+    $(".content_researchers").hide();
+  });
+
+  $(".teachers").click(function() {
+    $(".content_teachers").show();
+    $(".content_kids").hide();
+    $(".content_researchers").hide();
+  });
+
+  $(".researchers").click(function() {
+    $(".content_researchers").show();
+    $(".content_teachers").hide();
+    $(".content_kids").hide();
+  });
+
+  $(".support").click(function() {
+    $(".content_support").show();
+    $(".content_volunteer").hide();
+    $(".content_internships").hide();
+  });
+
+  $(".volunteer").click(function() {
+    $(".content_volunteer").show();
+    $(".content_support").hide();
+    $(".content_internships").hide();
+  });
+
+  $(".internships").click(function() {
+    $(".content_internships").show();
+    $(".content_volunteer").hide();
+    $(".content_support").hide();
+  });
+});
